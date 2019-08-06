4e602f7daee14829da1cbf5e2eb3f48f72ead6b8
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-08-06 11:16:45 +0200

    added alert message functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+//-----------ALERT MESSAGE-----------//
+
+const alert= document.querySelector('#alert');
+
+alert.innerHTML= `<div class="alert-banner">
+                  <p class="alert-message"><strong> Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+                  <p class="alert-banner-close">x</p>
+                  </div>`
+
+alert.addEventListener('click', e=>{
+  const element = e.target;
+  if(element.classList.contains("alert-banner-close")){
+    alert.style.display="none";
+  }
+});
 
 //-----------NAV BAR INTERACTION-----------//
 
