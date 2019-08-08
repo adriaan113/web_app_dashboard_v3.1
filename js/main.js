84da08c23efe5ca76bca557291c8347b95a7c654
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-08-08 14:48:20 +0200

    added js functionality to form

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
+
 //-----------NOTIFICATION ICON MENU-----------//
 
 const icon= document.querySelector('.icon > img');
 const iconAfter= document.querySelector('.notification-dot');
+
+const messageClose= document.querySelectorAll('.icon-menu-close');
+const message= document.querySelectorAll('.icon-menu-items');
 
 icon.addEventListener('click', ()=>{
   iconAfter.style.display='none';
 });
 
 
+
+function iconMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+document.addEventListener('click',(e)=>{
+  if(e.target.classList.contains('icon-menu-close')){
+    e.target.parentNode.style.display= 'none';
+
+    }else if(!event.target.matches('.dropbtn')){
+      const dropdowns = document.getElementsByClassName("dropdown-content");
+          for (let i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+});
 
 
 //-----------ALERT MESSAGE-----------//
@@ -44,6 +68,16 @@ navSelect.addEventListener('click',(e)=>{
   }
 });
 
+//-----------MOBILE NAV BAR INTERACTION-----------//
+
+// mobileBtn=document.querySelector('.nav-small');
+// mobileImg= document.querySelector('.nav-small > img');
+//
+// mobileBtn.addEventListener('click',()=>{
+//   e.target.className='';
+//   e.target.style.transform='rotate(90deg)';
+// });
+
 
 //-----------TRAFFIC INTERACTION-----------//
 
@@ -58,3 +92,173 @@ for (let i = 0; i < tNavLink.length; i++) {
     this.className += " active";
   });
 }
+
+
+//-----------SEND FORM-----------//
+
+const sendBtn= document.querySelector('#message-form > button');
+let sendPopUp=  document.createElement("DIV");
+const messageUser= document.querySelector('#message-form');
+
+const searchForUser= document.querySelector('#user-search');
+const messageForUser= document.querySelector('#user-message');
+
+sendPopUp.className='send-pop-up';
+
+
+messageUser.appendChild(sendPopUp);
+messageUser.insertBefore(sendPopUp, sendBtn);
+
+// setTimeout(function(){
+//     sendPopUp.style.display='none';
+// }, 3000);
+
+sendBtn.addEventListener('click', ()=>{
+
+if(searchForUser.value.length === 0) {
+
+    sendPopUp.classList.add('error');
+
+  sendPopUp.innerHTML= `
+                        <p class="send-pop-up-message">User not selected</p>
+                        <p class="send-pop-up-close">x</p>
+                        `
+  sendPopUp.style.display='flex';
+
+}else if(messageForUser.value.length === 0){
+
+    sendPopUp.classList.add('error');
+
+  sendPopUp.innerHTML= `
+                        <p class="send-pop-up-message">No message to send!</p>
+                        <p class="send-pop-up-close">x</p>
+                        `
+  sendPopUp.style.display='flex';
+}
+else{
+
+  sendPopUp.classList.remove('error');
+  sendPopUp.classList.add('succes');
+
+  sendPopUp.innerHTML= `
+                        <p class="send-pop-up-message">Message on it's way!</p>
+                        <p class="send-pop-up-close">x</p>
+                        `
+  sendPopUp.style.display='flex';
+}
+
+
+
+});
+
+sendPopUp.addEventListener('click', e=>{
+  const element = e.target;
+  if(element.classList.contains("send-pop-up-close")){
+    sendPopUp.style.display="none";
+  }
+});
+
+//-----------AUTOCOMPLETE INPUT FIELD-----------//
+//copied from w3schools//
+
+let users=["indiana jones","guust flater","kapitein haddock","duke nukem"];
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      messageUser.insertBefore(a,messageForUser);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+              b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+/*execute a function when someone clicks in the document:*/
+document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+});
+}
+
+autocomplete(document.getElementById("user-search"), users);
