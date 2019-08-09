82f8101fd8730d5f45e3e3a5b77b6c79af745680
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-08-09 15:24:53 +0200

    final check

--- js/main.js
+++ js/main.js
@@ -57,7 +57,6 @@ const navItems= document.querySelectorAll('nav > a');
 
 navSelect.addEventListener('click',(e)=>{
   for(let i=0;i<navItems.length;i++){
-    navItems[0].className='nav-small';
     navItems[i].className='';
 
     if(e.target.tagName==='A'){
@@ -68,20 +67,9 @@ navSelect.addEventListener('click',(e)=>{
   }
 });
 
-//-----------MOBILE NAV BAR INTERACTION-----------//
-
-// mobileBtn=document.querySelector('.nav-small');
-// mobileImg= document.querySelector('.nav-small > img');
-//
-// mobileBtn.addEventListener('click',()=>{
-//   e.target.className='';
-//   e.target.style.transform='rotate(90deg)';
-// });
-
 
 //-----------TRAFFIC INTERACTION-----------//
 
-//const tNav= document.querySelector('.t-nav');
 const tNavLink= document.querySelectorAll('.t-nav-link');
 
 
@@ -164,90 +152,60 @@ sendPopUp.addEventListener('click', e=>{
 let users=["indiana jones","guust flater","kapitein haddock","duke nukem"];
 
 function autocomplete(inp, arr) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
   var currentFocus;
-  /*execute a function when someone writes in the text field:*/
   inp.addEventListener("input", function(e) {
       var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
       closeAllLists();
       if (!val) { return false;}
       currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
       a = document.createElement("DIV");
       a.setAttribute("id", this.id + "autocomplete-list");
       a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
       this.parentNode.appendChild(a);
       messageUser.insertBefore(a,messageForUser);
-      /*for each item in the array...*/
       for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
           b = document.createElement("DIV");
-          /*make the matching letters bold:*/
           b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
           b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
           b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
               b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
               inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
               closeAllLists();
           });
           a.appendChild(b);
         }
       }
   });
-  /*execute a function presses a key on the keyboard:*/
   inp.addEventListener("keydown", function(e) {
       var x = document.getElementById(this.id + "autocomplete-list");
       if (x) x = x.getElementsByTagName("div");
       if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
         currentFocus++;
-        /*and and make the current item more visible:*/
         addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
+      } else if (e.keyCode == 38) {
         currentFocus--;
-        /*and and make the current item more visible:*/
         addActive(x);
       } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
         e.preventDefault();
         if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
           if (x) x[currentFocus].click();
         }
       }
   });
   function addActive(x) {
-    /*a function to classify an item as "active":*/
     if (!x) return false;
-    /*start by removing the "active" class on all items:*/
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
     if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
     x[currentFocus].classList.add("autocomplete-active");
   }
   function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
     for (var i = 0; i < x.length; i++) {
       x[i].classList.remove("autocomplete-active");
     }
   }
   function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
@@ -255,7 +213,7 @@ function autocomplete(inp, arr) {
     }
   }
 }
-/*execute a function when someone clicks in the document:*/
+
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
 });
@@ -266,15 +224,12 @@ autocomplete(document.getElementById("user-search"), users);
 
 //-----------SAVE SETTINGS TO LOCAL STORAGE-----------//
 
-
-
 const emailCheckbox= document.querySelector('#notifications');
 const publicCheckbox= document.querySelector('#public');
 let emailSet= localStorage.getItem("email");
 let publicSet= localStorage.getItem("public");
 const timeZone = document.querySelector('#timezone');
 let timeZoneSet= localStorage.getItem("timeZone", timeZone.value);
-
 
 
 emailCheckbox.addEventListener('click',()=>{
@@ -296,9 +251,6 @@ publicCheckbox.addEventListener('click',()=>{
 timeZone.addEventListener('input',()=>{
   localStorage.setItem('timeZone', timeZone.value);
 });
-
-
-
 
 document.addEventListener('DOMContentLoaded',()=>{
   if (emailSet === "on"){
